84a3fd86820e481b089850d7b5ee08f07f52ddbe
Mikhail Borisov <mikhail.boriso2014@gmail.com>
2026-01-31 19:01:28 +0500

    CLR project

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -1,31 +1,30 @@
 use optimizers::{
-    default_algorithms_params::ALGORITHMS, functions::TEST_FUNCTIONS,
+    default_algorithms_params::DEFAULT_ANSR, functions::TEST_FUNCTIONS,
     runner::run_multiple_optimizaions,
 };
 
 fn main() {
-    if let Some(optimizer) = ALGORITHMS[1].algorithm.as_zero_gradient() {
-        let functions = &TEST_FUNCTIONS;
-        let dimension_count = 16;
-        let maxiter = 10_000_000;
-        let seed_count = 200;
-        let stop_residual = 0.01;
-        let result = run_multiple_optimizaions(
-            optimizer,
-            functions,
-            dimension_count,
-            maxiter,
-            seed_count,
-            stop_residual,
-        );
-        println!(
-            "mean: {}, shifted_sphere: {}, shifted_weierstrass: {}, hilly: {}, forest: {}, megacity: {}",
-            result["mean"],
-            result["shifted_sphere"],
-            result["shifted_weierstrass"],
-            result["hilly"],
-            result["forest"],
-            result["megacity"]
-        );
-    }
+    let optimizer = &DEFAULT_ANSR;
+    let functions = &TEST_FUNCTIONS;
+    let dimension_count = 16;
+    let maxiter = 100_000;
+    let seed_count = 200;
+    let stop_residual = 0.01;
+    let result = run_multiple_optimizaions(
+        optimizer,
+        functions,
+        dimension_count,
+        maxiter,
+        seed_count,
+        stop_residual,
+    );
+    println!(
+        "mean: {}, shifted_sphere: {}, shifted_weierstrass: {}, hilly: {}, forest: {}, megacity: {}",
+        result["mean"],
+        result["shifted_sphere"],
+        result["shifted_weierstrass"],
+        result["hilly"],
+        result["forest"],
+        result["megacity"]
+    );
 }
--- src/bin/plot.rs
+++ src/bin/plot.rs
@@ -1,5 +1,5 @@
 use optimizers::{
-    default_algorithms_params::ALGORITHMS,
+    default_algorithms_params::DEFAULT_ANSR,
     early_stop_callback::EarlyStopCallback,
     functions::TEST_FUNCTIONS,
     optimizer::Optimizer,
@@ -8,30 +8,29 @@ use optimizers::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Some(optimizer) = ALGORITHMS[1].algorithm.as_zero_gradient() {
-        let function = &TEST_FUNCTIONS[2];
-        let dimension_count = 10;
-        let maxiter = 10_000;
-        let stop_residual = 0.01;
-        let func = &broadcast(&function.func);
-        let bounds = &function.bounds.repeat(dimension_count / 2);
-        let early_stop_callback = EarlyStopCallback::new(func, stop_residual);
-        let result = optimizer.find_infimum(func, bounds, maxiter, 42, true, &early_stop_callback);
-        println!("f({:?}) == {:?}, nfev={}", result.x, result.f_x, result.nfev);
-        if let Some(history) = result.history {
-            save_video_h264(
-                func,
-                &format_x_history(&history.x, bounds),
-                &format_best_f_x_history(&history.f_x),
-                "result.mp4",
-                bounds[0],
-                bounds[1],
-                1024,
-                1024,
-                1024,
-                30,
-            )?;
-        }
+    let optimizer = &DEFAULT_ANSR;
+    let function = &TEST_FUNCTIONS[4];
+    let dimension_count = 10_000;
+    let maxiter = 100_000;
+    let stop_residual = 0.01;
+    let func = &broadcast(&function.func);
+    let bounds = &function.bounds.repeat(dimension_count / 2);
+    let early_stop_callback = EarlyStopCallback::new(func, stop_residual);
+    let result = optimizer.find_infimum(func, bounds, maxiter, 42, true, &early_stop_callback);
+    println!("f() == {:?}, nfev={}", result.f_x, result.nfev);
+    if let Some(history) = result.history {
+        save_video_h264(
+            func,
+            &format_x_history(&history.x, bounds),
+            &format_best_f_x_history(&history.f_x),
+            "result.mp4",
+            bounds[0],
+            bounds[1],
+            1024,
+            1024,
+            1024,
+            30,
+        )?;
     }
     return Ok(());
 }
